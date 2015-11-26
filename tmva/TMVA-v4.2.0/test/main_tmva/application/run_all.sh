bc53e497a7df48531944be79db5365b0d345e7ba
Nadezda <nadezda.chernyavskaya@gmail.com>
2015-11-26 22:37:02 +0100

    new TMVA for v14

--- tmva/TMVA-v4.2.0/test/main_tmva/application/run_all.sh
+++ tmva/TMVA-v4.2.0/test/main_tmva/application/run_all.sh
@@ -21,7 +21,7 @@ mva=main_mva_v14_
 current_sample=0
 while [ $current_sample -lt 1 ]  #$max_samples_num ]
 do	
-#	qsub -q short.q batch2.sh $path${input_dir[ $current_sample ]}$double$ROOT $mva${input_dir[ $current_sample ]} 0
-	qsub -q short.q batch2.sh $path$qgd${input_dir[ $current_sample ]}$single$jets$ROOT $mva${input_dir[ $current_sample ]} 1
+	qsub -q short.q batch2.sh $path${input_dir[ $current_sample ]}$double$ROOT $mva${input_dir[ $current_sample ]} 0
+#	qsub -q short.q batch2.sh $path$qgd${input_dir[ $current_sample ]}$single$jets$ROOT $mva${input_dir[ $current_sample ]} 1
 	current_sample=$(( $current_sample + 1 ))
 done
